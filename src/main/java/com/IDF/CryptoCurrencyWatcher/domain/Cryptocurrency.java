eea6ba1236d0166a96f09ac594c8b45db0aa444f
lemonclouds <hannah.lakhai@gmail.com>
2022-07-08 15:21:40 +0300

    Add Cryptocurrency domain, repo, service & controller

---
+++ src/main/java/com/IDF/CryptoCurrencyWatcher/domain/Cryptocurrency.java
@@ -0,0 +1,28 @@
+package com.IDF.CryptoCurrencyWatcher.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tickers")
+public class Cryptocurrency {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "symbol", nullable = false)
+    private String symbol;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+}
---
+++ src/main/java/com/IDF/CryptoCurrencyWatcher/service/CryptocurrencyService.java
@@ -0,0 +1,37 @@
+package com.IDF.CryptoCurrencyWatcher.service;
+
+import com.IDF.CryptoCurrencyWatcher.domain.Cryptocurrency;
+import com.IDF.CryptoCurrencyWatcher.repository.CryptocurrencyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CryptocurrencyService {
+    private final CryptocurrencyRepository cryptocurrencyRepository;
+
+    public List<Cryptocurrency> findAll() {
+        return cryptocurrencyRepository.findAll();
+    }
+
+    public Cryptocurrency findById(Long id) {
+        return cryptocurrencyRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Cryptocurrency[%d] not found", id))
+        );
+    }
+
+    public Cryptocurrency findBySymbol(String symbol) {
+        return cryptocurrencyRepository.findBySymbol(symbol).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Cryptocurrency[%s] not found", symbol))
+        );
+    }
+
+
+
+    public Cryptocurrency update(Cryptocurrency cryptocurrency) {
+        return cryptocurrencyRepository.save(cryptocurrency);
+    }
+}
