50b5ab5d06128705abffae18a3ca35d133e6466c
lemonclouds <hannah.lakhai@gmail.com>
2022-07-08 16:21:37 +0300

    Add JsonParser class & updatePrices() method to CryptocurrencyService

--- src/main/java/com/IDF/CryptoCurrencyWatcher/service/CryptocurrencyService.java
+++ src/main/java/com/IDF/CryptoCurrencyWatcher/service/CryptocurrencyService.java
@@ -2,15 +2,27 @@ package com.IDF.CryptoCurrencyWatcher.service;
 
 import com.IDF.CryptoCurrencyWatcher.domain.Cryptocurrency;
 import com.IDF.CryptoCurrencyWatcher.repository.CryptocurrencyRepository;
+import com.IDF.CryptoCurrencyWatcher.util.JsonParser;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@EnableAsync
+@EnableScheduling
 public class CryptocurrencyService {
+
+    private final String COINLORE_URL = "https://api.coinlore.net/api/ticker/?id=%d";
+
     private final CryptocurrencyRepository cryptocurrencyRepository;
 
     public List<Cryptocurrency> findAll() {
@@ -29,9 +41,23 @@ public class CryptocurrencyService {
         );
     }
 
-
-
     public Cryptocurrency update(Cryptocurrency cryptocurrency) {
         return cryptocurrencyRepository.save(cryptocurrency);
+    }
+
+    @Async
+    @Scheduled(fixedRate = 6000)
+    public void updatePrices() {
+        findAll()
+                .forEach(cryptocurrency -> {
+                    try {
+                        JSONObject jsonObject = JsonParser.readJsonFromUrl(
+                                String.format(COINLORE_URL, cryptocurrency.getId()));
+                        cryptocurrency.setPrice(Double.parseDouble(jsonObject.get("price_usd").toString()));
+                        update(cryptocurrency);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
